bc7138395ec67425b63e4cfd6bc5f564c6f42dcd
Isabel <202301114@alu.comillas.edu.com>
2026-02-12 09:38:16 +0100

    Creación del Record Usuario

---
+++ src/main/java/RECORDS/Usuario.java
@@ -0,0 +1,46 @@
+package RECORDS;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record Usuario(
+
+        @NotBlank
+        String idUsuario,
+
+        @NotBlank
+        String nombre,
+
+        @NotBlank
+        String apellidos,
+
+        @NotBlank
+        @Email
+        String email,         
+
+        @NotBlank
+        String password,
+
+        @NotBlank
+        String telefono,
+
+        @NotNull
+        Rol rol,
+
+        @NotNull
+        LocalDateTime fechaRegistro,
+
+        boolean activo,
+
+        List<Reserva> reservas  // Lista de Reservas que tiene asignadas el usuario
+
+) {
+
+    public enum Rol { //Para que Rol solo pueda tomar valores USER o ADMIN
+        USER,
+        ADMIN
+    }
+}
