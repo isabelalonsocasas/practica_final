2a711909d3b2b3afc848a925e2c7df70d1930bf2
jmfernandezfreige <202311871@alu.comillas.edu>
2026-02-12 10:23:28 +0100

    Añadir record Pista

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/RECORDS/Pista.java
@@ -0,0 +1,28 @@
+package edu.comillas.icai.gitt.pat.spring.practica_final.RECORDS;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.time.LocalDateTime;
+
+public record Pista(
+    @PositiveOrZero @NotNull
+    Integer idPista,
+
+    @NotBlank
+    String nombre,
+
+    @NotBlank
+    String ubicacion,
+
+    @Positive
+    double precioHora,
+
+    boolean activa,
+
+    @NotNull
+    LocalDateTime fechaAlta
+
+) {}
