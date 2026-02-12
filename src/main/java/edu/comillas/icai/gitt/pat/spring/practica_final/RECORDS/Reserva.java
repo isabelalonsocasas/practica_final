93fea21d9efd84a0d1517fb2f4272d8c9c8938b4
jmfernandezfreige <202311871@alu.comillas.edu>
2026-02-12 10:33:41 +0100

    Añadir record Reserva

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/RECORDS/Reserva.java
@@ -0,0 +1,46 @@
+package edu.comillas.icai.gitt.pat.spring.practica_final.RECORDS;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public record Reserva(
+        @PositiveOrZero
+        int idReserva,
+
+        @PositiveOrZero
+        int idUsuario,
+
+        @PositiveOrZero
+        int idPista,
+
+        @NotNull
+        LocalDateTime fechaReserva,
+
+        @NotNull
+        LocalTime horaInicio,
+
+        @NotNull
+        int duracionMinutos,
+
+        LocalTime horaFin,
+
+        @NotNull
+        Estado estado,
+
+        @NotNull
+        LocalDateTime fechaCreacion
+) {
+    public enum Estado {
+        ACTIVA,
+        CANCELADA
+    }
+
+    public Reserva {
+        if (horaFin == null) {
+            horaFin = horaInicio().plusMinutes(duracionMinutos);
+        }
+    }
+}
