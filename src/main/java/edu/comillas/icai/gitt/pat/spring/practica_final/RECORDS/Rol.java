091c065a95fc6a5154022ddc8004e2654d405e98
jmfernandezfreige <202311871@alu.comillas.edu>
2026-02-12 10:40:03 +0100

    Añadir Record Rol

--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/RECORDS/Rol.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/RECORDS/Rol.java
@@ -2,7 +2,6 @@ package edu.comillas.icai.gitt.pat.spring.practica_final.RECORDS;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record Rol(
