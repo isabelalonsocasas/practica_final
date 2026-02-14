6211a2978e0c847cae7650640ffd5e3bf76167ce
Isabel <202301114@alu.comillas.edu.com>
2026-02-14 12:33:11 +0100

    Metodo get usuario autenticado

--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/PadelControlador.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/PadelControlador.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
