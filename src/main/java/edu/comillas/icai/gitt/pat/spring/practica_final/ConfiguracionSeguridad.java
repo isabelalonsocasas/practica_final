90b9bd71115dea360f508f5c28a84f1a2086a43d
Isabel <202301114@alu.comillas.edu.com>
2026-02-14 11:47:36 +0100

    Cambios seguridad

--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/ConfiguracionSeguridad.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/ConfiguracionSeguridad.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 
 
