4a3c4cda696b2159a845be80ed1113d4b5fa79ff
bibidoradom <202302724@alu.comillas.edu>
2026-03-14 15:35:21 +0100

    comienzo servicios

--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/servicio/ServicioReservas.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/servicio/ServicioReservas.java
@@ -1,4 +1,20 @@
 package edu.comillas.icai.gitt.pat.spring.practica_final.servicio;
 
+import edu.comillas.icai.gitt.pat.spring.practica_final.repositorio.RepoPista;
+import edu.comillas.icai.gitt.pat.spring.practica_final.repositorio.RepoReserva;
+import edu.comillas.icai.gitt.pat.spring.practica_final.repositorio.RepoRol;
+import edu.comillas.icai.gitt.pat.spring.practica_final.repositorio.RepoUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServicioReservas {
+    @Autowired
+    RepoPista repoPista;
+    @Autowired
+    RepoReserva repoReserva;
+    @Autowired
+    RepoRol repoRol;
+    @Autowired
+    RepoUsuario repoUsuario;
 }
