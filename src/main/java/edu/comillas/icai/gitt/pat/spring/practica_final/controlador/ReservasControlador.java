2b038929baff60755ad071b763996292e612d3c3
bibidoradom <202302724@alu.comillas.edu>
2026-03-14 15:37:37 +0100

    comienzo servicios y controllers

--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/controlador/ReservasControlador.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/controlador/ReservasControlador.java
@@ -1,4 +1,17 @@
 package edu.comillas.icai.gitt.pat.spring.practica_final.controlador;
 
+import edu.comillas.icai.gitt.pat.spring.practica_final.servicio.ServicioPistas;
+import edu.comillas.icai.gitt.pat.spring.practica_final.servicio.ServicioReservas;
+import edu.comillas.icai.gitt.pat.spring.practica_final.servicio.ServicioUsuarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ReservasControlador {
+    @Autowired
+    ServicioPistas servicioPistas;
+    @Autowired
+    ServicioUsuarios servicioUsuarios;
+    @Autowired
+    ServicioReservas servicioReservas;
 }
--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/controlador/UsuarioControlador.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/controlador/UsuarioControlador.java
@@ -1,4 +1,42 @@
 package edu.comillas.icai.gitt.pat.spring.practica_final.controlador;
 
+import edu.comillas.icai.gitt.pat.spring.practica_final.RECORDS.Usuario;
+import edu.comillas.icai.gitt.pat.spring.practica_final.servicio.ServicioPistas;
+import edu.comillas.icai.gitt.pat.spring.practica_final.servicio.ServicioReservas;
+import edu.comillas.icai.gitt.pat.spring.practica_final.servicio.ServicioUsuarios;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
 public class UsuarioControlador {
+    @Autowired
+    ServicioPistas servicioPistas;
+    @Autowired
+    ServicioUsuarios servicioUsuarios;
+    @Autowired
+    ServicioReservas servicioReservas;
+    ///  Métodos auth usuario
+
+    //Registrarse (completado)
+    @PostMapping("/pistaPadel/auth/register")
+    public ResponseEntity<Usuario> registrarUsuario(@Valid @RequestBody Usuario NuevoUsuario) {
+        boolean emailExiste = almacen.usuarios().values().stream()
+                .anyMatch(u -> u.email().equals(NuevoUsuario.email()));
+
+        if (emailExiste) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "El email ya existe"
+            );
+        }
+        almacen.usuarios().put(NuevoUsuario.idUsuario(), NuevoUsuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(NuevoUsuario); //Refleja 201 created y el usuario
+
+    }
 }
