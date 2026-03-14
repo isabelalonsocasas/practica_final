ed80ed6fc23f6496f573b8621200b93cf5062f5c
jmfernandezfreige <202311871@alu.comillas.edu>
2026-03-14 13:36:41 +0100

    Corregir anotacion

--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/repositorio/RepoUsuario.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica_final/repositorio/RepoUsuario.java
@@ -2,9 +2,7 @@ package edu.comillas.icai.gitt.pat.spring.practica_final.repositorio;
 
 import edu.comillas.icai.gitt.pat.spring.practica_final.entidad.Usuario;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RepoUsuario extends CrudRepository<Usuario, Long> {
     Usuario findByEmail(String email);
 }
