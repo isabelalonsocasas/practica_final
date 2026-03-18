132baa4ed9e1ca4c5336e7b467d13f9095d4dcac
bibidoradom <202302724@alu.comillas.edu>
2026-03-18 23:47:34 +0100

    tests

--- src/test/java/edu/comillas/icai/gitt/pat/spring/practica_final/PracticaFinalApplicationTests.java
+++ src/test/java/edu/comillas/icai/gitt/pat/spring/practica_final/PracticaFinalApplicationTests.java
@@ -1,13 +1,41 @@
 package edu.comillas.icai.gitt.pat.spring.practica_final;
 
+import edu.comillas.icai.gitt.pat.spring.practica_final.entidad.Pista;
+import edu.comillas.icai.gitt.pat.spring.practica_final.repositorio.RepoPista;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-class PracticaFinalApplicationTests {
+class PracticaFinalApplicationTest{
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private RepoPista pistaRepository;
+
+	@Test
+	void listarPistas_conDatos() {
+		Pista pista = new Pista();
+		pistaRepository.save(pista);
+
+		ResponseEntity<Pista[]> response =
+				restTemplate.getForEntity("/pistaPadel/courts?active=true", Pista[].class);
+
+		assertEquals(200, response.getStatusCodeValue());
+		assertTrue(response.getBody().length > 0);
 	}
 
 }
