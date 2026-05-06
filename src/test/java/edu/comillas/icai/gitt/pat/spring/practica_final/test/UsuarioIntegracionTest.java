623535c5ff56485254a3376b519b343b221a3f73
bibidoradom <bibidm212@gmail.com>
2026-05-06 19:17:38 +0200

    test integracion

--- src/test/java/edu/comillas/icai/gitt/pat/spring/practica_final/test/UsuarioIntegracionTest.java
+++ src/test/java/edu/comillas/icai/gitt/pat/spring/practica_final/test/UsuarioIntegracionTest.java
@@ -8,12 +8,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.http.*;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Para integración y E2E utilizamos RestTemplate y no mocl porque no queremos simular nada,
+// queremos comprobar la conexión real entre capas.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 class UsuarioIntegracionTest {
 
     @Autowired
